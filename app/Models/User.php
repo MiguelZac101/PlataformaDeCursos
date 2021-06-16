c95fd71488a3e186135d1e1c97abf054d78fd719
Miguel Zac <miguel.qzac@gmail.com>
2021-06-16 15:26:14 -0500

    28. Instalar Laravel Permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,8 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+//laravel permission
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
@@ -17,6 +19,8 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    //laravel permission
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
