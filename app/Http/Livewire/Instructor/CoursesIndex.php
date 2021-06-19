9912a198549a82f5b743fb8917b1ab9f7630d1df
Miguel Zac <miguel.qzac@gmail.com>
2021-06-19 11:35:10 -0500

    34. Reorganizar rutas

--- app/Http/Livewire/Instructor/CoursesIndex.php
+++ app/Http/Livewire/Instructor/CoursesIndex.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Http\Livewire;
+namespace App\Http\Livewire\Instructor;
 
 use Livewire\Component;
 use App\Models\Course;
 
 use Livewire\WithPagination;
 
-class InstructorCourses extends Component
+class CoursesIndex extends Component
 {
     use WithPagination;
 
@@ -17,7 +17,7 @@ class InstructorCourses extends Component
     {
         //$courses = Course::where('user_id',auth()->user()->id)->get();
         $courses = Course::where('title','LIKE','%'.$this->search.'%')->paginate(10);
-        return view('livewire.instructor-courses',compact('courses'));
+        return view('livewire.instructor.courses-index',compact('courses'));
     }
 
     public function limpiar_page(){
--- routes/instructor.php
+++ routes/instructor.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Livewire\InstructorCourses;
+//use App\Http\Livewire\InstructorCourses;
+use App\Http\Controllers\Instructor\CourseController;
+
 
 //redirigir ruta
 Route::redirect('', 'instructor/courses');
 //middleware restrige ruta por permiso can:nombrepermiso
-Route::get('courses', InstructorCourses::class)->middleware('can:Leer Cursos')->name('courses.index');
+//Route::get('courses', InstructorCourses::class)->middleware('can:Leer Cursos')->name('courses.index');
+Route::resource('courses', CourseController::class)->names('courses');
