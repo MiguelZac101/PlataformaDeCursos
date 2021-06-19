dba9fe9a418b8be927663525c9ee9a34f87f8f3b
Miguel Zac <miguel.qzac@gmail.com>
2021-06-19 10:53:10 -0500

    33. Instructor:listado de cursos. livewire:pagination, blade component

---
+++ resources/views/components/instructor-course-tr.blade.php
@@ -0,0 +1,12 @@
+@props(['course'])
+<tr>
+    <td>
+        <img src="{{Storage::url($course->image->url)}}" alt="" width="50">
+    </td>
+    <td>{{$course->title}}</td>
+    <td>{{$course->category->name}}</td>
+    <td>{{$course->students->count()}}</td>
+    <td>{{$course->rating}}</td>
+    <td>{{$course->status}}</td>
+    <td>{{$slot}}</td>
+</tr>
--- resources/views/livewire/instructor-courses.blade.php
+++ resources/views/livewire/instructor-courses.blade.php
@@ -1,3 +1,31 @@
 <div>
-    {{-- Do your work, then step back. --}}
+    <div>
+        <input wire:model="search" wire:keydown="limpiar_page" type="text">
+    </div>
+    
+    @if ($courses->count())
+    <table>
+        <tr>
+            <th>image</th>
+            <th>Title</th>
+            <th>category</th>
+            <th>matriculados</th>
+            <th>rating</th>
+            <th>Status</th>
+            <th>@ slot</th>
+        </tr>
+        @foreach ($courses as $course)
+            <x-instructor-course-tr :course="$course">
+                contenido de @ slot
+            </x-instructor-course-tr>
+        @endforeach
+        <tr>
+            <td colspan="7">{{$courses->links()}}</td>
+        </tr>
+    </table>     
+    @else
+        <div>
+            No hay registros..        
+        </div>
+    @endif
 </div>
