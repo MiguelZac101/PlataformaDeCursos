fdbc9221c895d291147b3b8af9dcdba16f89bbe7
Miguel Zac <miguel.qzac@gmail.com>
2021-06-19 19:12:26 -0500

    instructor/courses, mejora tbl

--- resources/views/components/instructor-course-tr.blade.php
+++ resources/views/components/instructor-course-tr.blade.php
@@ -1,15 +1,40 @@
 @props(['course'])
 <tr>
-    <td>
-        <img src="{{Storage::url($course->image->url)}}" alt="" width="50">
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <div class="flex items-center">
+            <div class="flex-shrink-0 w-10 h-10">
+                <img class="w-full h-full rounded-full"
+                    src="{{Storage::url($course->image->url)}}"
+                    alt="" />
+            </div>                                                
+        </div>
     </td>
-    <td>{{$course->title}}</td>
-    <td>{{$course->category->name}}</td>
-    <td>{{$course->students->count()}}</td>
-    <td>{{$course->rating}}</td>
-    <td>{{$course->status}}</td>
-    <td>{{$slot}}</td>
-    <td>
-        <a href="{{route('instructor.courses.edit',$course)}}">Editar</a>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p class="text-gray-900 whitespace-no-wrap">{{$course->title}}</p>
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p class="text-gray-900 whitespace-no-wrap">
+            {{$course->category->name}}
+        </p>
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p class="text-gray-900 whitespace-no-wrap">
+            {{$course->students->count()}}
+        </p>
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p class="text-gray-900 whitespace-no-wrap">
+            {{$course->rating}}
+        </p>
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p class="text-gray-900 whitespace-no-wrap">
+            {{$course->status}}
+        </p>
+    </td>
+    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p class="text-gray-900 whitespace-no-wrap">
+            <a href="{{route('instructor.courses.edit',$course)}}">Editar</a>
+        </p>
     </td>
 </tr>
--- resources/views/livewire/instructor/courses-index.blade.php
+++ resources/views/livewire/instructor/courses-index.blade.php
@@ -1,32 +1,77 @@
 <div>
-    <div>
-        <input wire:model="search" wire:keydown="limpiar_page" type="text">
-    </div>
-    
-    @if ($courses->count())
-    <table>
-        <tr>
-            <th>image</th>
-            <th>Title</th>
-            <th>category</th>
-            <th>matriculados</th>
-            <th>rating</th>
-            <th>Status</th>
-            <th>@ slot</th>
-            <th></th>            
-        </tr>
-        @foreach ($courses as $course)
-            <x-instructor-course-tr :course="$course">
-                contenido de @ slot
-            </x-instructor-course-tr>
-        @endforeach
-        <tr>
-            <td colspan="8">{{$courses->links()}}</td>
-        </tr>
-    </table>     
-    @else
-        <div>
-            No hay registros..        
+    <!-- component -->
+    <div class="container mx-auto px-4 sm:px-8">
+        <div class="py-8">
+            <div>
+                <h2 class="text-2xl font-semibold leading-tight">Courses</h2>
+            </div>
+            <div class="my-2 flex sm:flex-row flex-col">                
+                <div class="block relative">
+                    <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                        <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
+                            <path
+                                d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
+                            </path>
+                        </svg>
+                    </span>
+                    <input wire:model="search" wire:keydown="limpiar_page" placeholder="Search" class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+                </div>
+            </div>
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                    
+                    <table class="min-w-full leading-normal">
+                        <thead>
+                            <tr>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Imagen
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Titulo
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    categoria
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    matriculados
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    rating
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    status
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Editar
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if ($courses->count())
+                                @foreach ($courses as $course)
+                                    <x-instructor-course-tr :course="$course"/>
+                                @endforeach
+                            @else
+                                <td colspan="7" class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">
+                                        No hay registros.. 
+                                    </p>
+                                </td>                                
+                            @endif                            
+                        </tbody>
+                    </table>
+                    <div class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between">
+                        {{$courses->links()}}
+                    </div>
+                    
+                </div>
+            </div>
         </div>
-    @endif
-</div>
+    </div>
