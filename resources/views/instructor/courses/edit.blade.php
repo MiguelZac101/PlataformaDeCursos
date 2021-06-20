179cc25893dc8401b3d28fa3bfc0e965bf678cbd
Miguel Zac <miguel.qzac@gmail.com>
2021-06-20 16:56:14 -0500

    41. Crear nueva plantilla para instructores. Blade::directive('routeIs'..., Component Blade

--- resources/views/instructor/courses/edit.blade.php
+++ resources/views/instructor/courses/edit.blade.php
@@ -1,43 +1,26 @@
-<x-app-layout>
-    <div class="container py-8 grid grid-cols-5">
-        <aside>
-            <h1 class="font-bold text-lg mb-4">Edicion de curso</h1>
-            <ul>
-                <li class="leading-7 mb-1 border-1-4">
-                    <a href="">Información del curso</a>
-                </li>
-                <li class="leading-7 mb-1 border-1-4">
-                    <a href="">Lecciones del curso</a>
-                </li>
-                <li class="leading-7 mb-1 border-1-4">
-                    <a href="">Metas del curso</a>
-                </li>
-                <li class="leading-7 mb-1 border-1-4">
-                    <a href="">Estudiantes</a>
-                </li>
-            </ul>
-        </aside>
-        <div class="col-span-4 card">
-            <div class="card-body text-gray-600">
-                <h1 class="text-2x1 font-bold">INFORMACIÓN DEL CURSO</h1>
-                <hr>
-                {!! Form::model($course, ['route'=>['instructor.courses.update',$course],'method'=>'put','files'=>true]) !!}
+<x-instructor-layout>
 
-                    @include('instructor.courses.partials.form')
+    <x-slot name="course">
+        {{$course->slug}}
+    </x-slot>
+    
+    <h1 class="text-2x1 font-bold">INFORMACIÓN DEL CURSO</h1>
+    <hr>
+    {!! Form::model($course, ['route'=>['instructor.courses.update',$course],'method'=>'put','files'=>true]) !!}
 
-                    <div class="flex justify-end">
-                        {!! Form::submit('Actualizar información', ['class'=>'btn btn-primary']) !!}
-                    </div>
+        @include('instructor.courses.partials.form')
 
-                {!! Form::close() !!}
-            </div>
+        <div class="flex justify-end">
+            {!! Form::submit('Actualizar información', ['class'=>'btn btn-primary']) !!}
         </div>
-    </div>
+
+    {!! Form::close() !!}
+
 
     <!--slot con nombre,renderiza contenido en views/layouts/app.blade.php-->
     <x-slot name="js">
-
         <script src="https://cdn.ckeditor.com/ckeditor5/28.0.0/classic/ckeditor.js"></script>
         <script src="{{asset('js/instructor/form.js')}}"></script>
     </x-slot>
-</x-app-layout>
+    
+</x-instructor-layout>
--- routes/instructor.php
+++ routes/instructor.php
@@ -4,9 +4,12 @@ use Illuminate\Support\Facades\Route;
 //use App\Http\Livewire\InstructorCourses;
 use App\Http\Controllers\Instructor\CourseController;
 
+use App\Http\Livewire\Instructor\CoursesCurriculum;
 
 //redirigir ruta
 Route::redirect('', 'instructor/courses');
 //middleware restrige ruta por permiso can:nombrepermiso
 //Route::get('courses', InstructorCourses::class)->middleware('can:Leer Cursos')->name('courses.index');
 Route::resource('courses', CourseController::class)->names('courses');
+
+Route::get('courses/{course}/curriculum',CoursesCurriculum::class)->name('courses.curriculum');
